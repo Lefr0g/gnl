1c293f63648a75225e7a272448f00a8cfae60548
Antoine MULIN <antoine.mulin@gmail.com>
2015-02-10 16:32:56 +0100

    GNL ok, improving main.c to add support for multiple fd

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/19 10:04:06 by amulin            #+#    #+#             */
-/*   Updated: 2015/02/07 16:02:31 by amulin           ###   ########.fr       */
+/*   Updated: 2015/02/10 14:42:34 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 65
+# define BUFF_SIZE 32
 # include "libft.h"
 # include <unistd.h>
 
--- main.c
+++ main.c
@@ -6,14 +6,30 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/05 16:27:47 by amulin            #+#    #+#             */
-/*   Updated: 2015/02/07 12:46:18 by amulin           ###   ########.fr       */
+/*   Updated: 2015/02/10 16:30:43 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <fcntl.h>
 #include "get_next_line.h"
-#include <stdio.h>
+
+int	gnl_loop(int fd, char **line)
+{
+	while (get_next_line(fd, line))
+		ft_putendl(*line);
+	if (get_next_line(0, line) == -1)
+	{
+		ft_putstr("Main >> GNL returned 0, read over\n");
+		return (0);
+	}
+	if (get_next_line(0, line) == -1)
+	{
+		ft_putstr("Main >> GNL returned -1, error\n");
+		return (1);
+	}
+	return (0);
+}
 
 int	run_gnl(int fd, char **line)
 {
@@ -30,87 +46,121 @@ int	run_gnl(int fd, char **line)
 	{
 		ft_putstr("Main >> GNL run ");
 		ft_putnbr(i);
-		ft_putstr(", GNL returns 1, line read gives : '");
+		ft_putstr(", returns 1, line = \t'");
 		ft_putstr(*line);
 		ft_putstr("'\n");
 		ret = get_next_line(fd, line);
-//		printf("Main >> run_gnl loop, i = %d\n", i);
 		i++;
 	}
 	if (ret == 0)
 	{
 		ft_putstr("Main >> GNL run ");
 		ft_putnbr(i);
-		ft_putstr(", GNL returns 0, line read gives : '");
+		ft_putstr(", returns 0, line = \t'");
 		ft_putstr(*line);
 		ft_putstr("'\n");
 		ft_putstr("Main >> GNL returned 0, read over\n");
 	}
 	if (ret == -1)
-		ft_putstr("Main >> GNL returns -1, error\n");
+	{
+		ft_putstr("Main >> GNL returned -1, error\n");
+		return (1);
+	}
 	return (0);
 }
 
-void	run_read_stdin(void)
+int	run_gnl_stdin(char **line)
 {
-	int		buffsize;
-	int		ret;
-	char	*buff;
-
-	buffsize = 30;
-	buff = ft_strnew(buffsize);
-	ret = read(0, buff, buffsize);
-	printf("(TBDel)Main >> ret = %d\n", ret);
-	printf("(TBDel)Main >> buff contains '%s'\n", buff);
-}
-
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-
-	line = ft_strnew(BUFF_SIZE);
-	ft_strcpy(line, "Hello");
-	ft_putstr("Main >> Line variable initialized to '");
-	ft_putstr(line);
-	ft_putstr("'\n");
-	printf("Main >> Address of line is : %p\n", line);
-	if (argc != 2)
+	ft_putstr("Main >> Running GNL from stdin.\n");
+	ft_putstr("     >>> Type 'quit' to end program.\n");
+	ft_putstr("     >>> Type 'control-v control-j' to write an end-of-line.\n");
+	while (get_next_line(0, line))
 	{
-		if (argc == 1)
-			ft_putstr("Main >> Error : Please enter a parameter\n");
-		else
-			ft_putstr("Main >> Error : Too many parameters\n");
+		if (!ft_strcmp(*line, "quit"))
+			return (0);
+		ft_putendl(*line);
+	}
+	if (get_next_line(0, line) == -1)
+	{
+		ft_putstr("Main >> GNL returned 0, read over\n");
 		return (0);
 	}
+	if (get_next_line(0, line) == -1)
+	{
+		ft_putstr("Main >> GNL returned -1, error\n");
+		return (1);
+	}
+	return (0);
+}
+
+int file_opening(char *str, int *fd)
+{
 	ft_putstr("Main >> Opening file '");
-	ft_putstr(argv[1]);
+	ft_putstr(str);
 	ft_putstr("'...\n");
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	*fd = open(str, O_RDONLY);
+	if (*fd < 0)
 	{
 		ft_putstr("Main >> Open fail, end of test.\n");
 		return (1);
 	}
 	ft_putstr("Main >> File successfully opened on fd = ");
-	ft_putnbr(fd);
+	ft_putnbr(*fd);
 	ft_putchar('\n');
-	run_gnl(fd, &line);
-	ft_putstr("Main >> Closing file...\n");
-	if (close(fd) != 0)
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	int		i;
+	char	*line;
+
+//	line = ft_strnew(BUFF_SIZE);
+	line = "Hello";
+	i = 1;
+	if (argc == 1)
 	{
-		ft_putstr("Main >> Close failed, end of test.\n");
-		return (1);
+		ft_putstr("Main >> Error : Please enter a parameter.\n");
+		ft_putstr("     >>> Help : Try 'file_name' to read from a file, or 'stdin' from standard input.\n");
+		ft_putstr("     >>> Example : ./test_gnl fichier0\n");
 	}
-	ft_putstr("Main >> File successfully closed.\n");
-/*
-	ft_putstr("(TBDel)Main >> Read from stdin test start\n");
-	run_read_stdin();
-	ft_putstr("(TBDel)Main >> Read from stdin test over\n");
-	
-	ft_putstr("Main >> Attempting GNL on stdin.\n");
-	run_gnl(0, &line);
-	ft_putstr("Main >> Test completed, end of program.\n");
-*/
+	else if (argc == 2)
+	{
+		if (!ft_strcmp(argv[1], "stdin"))
+			run_gnl_stdin(&line);
+		else 
+		{
+			if (file_opening(argv[1], &fd))
+				return (1);
+			run_gnl(fd, &line);
+			ft_putstr("Main >> Closing file...\n");
+			if (close(fd) != 0)
+			{
+				ft_putstr("Main >> Close failed, end of test.\n");
+				return (1);
+			}
+			ft_putstr("Main >> File successfully closed.\n");
+			i++;
+		}
+	}
+	else
+	{
+		while (i < argc)
+		{
+			if (file_opening(argv[i], &fd))
+				return (1);
+			run_gnl(fd, &line);
+			ft_putstr("Main >> Closing file...\n");
+			if (close(fd) != 0)
+			{
+				ft_putstr("Main >> Close failed, end of test.\n");
+				return (1);
+			}
+			ft_putstr("Main >> File successfully closed.\n");
+			i++;
+		}
+	}
+	ft_putstr("Main >> All fine, end of test.\n");
 	return (0);
 }
